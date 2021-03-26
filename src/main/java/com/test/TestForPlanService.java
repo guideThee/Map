4dd338134393ba390765644e4d568334eb9333b5
guideThee <2469644018@qq.com>
2021-03-26 08:31:50 +0800

    加入TestForPlanService测试类

---
+++ src/main/java/com/test/TestForPlanService.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestForPlanService {
+
+	@Test
+	public void testAddPlanByPlan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryAllPlan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryByuserLoginnameAndplanName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteByuserLoginnameAndplanName() {
+		fail("Not yet implemented");
+	}
+
+}
